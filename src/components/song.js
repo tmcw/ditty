482d5e627f82815d6c634c92cb25a608191dc280
Tom MacWright <tom@macwright.org>
2015-02-17 15:03:23 -0500

    Increase range to 32x32

--- src/components/song.js
+++ src/components/song.js
@@ -10,21 +10,24 @@ var osc = T('pulse');
 var env = T('perc', {a:50, r:250});
 var synth = T('OscGen', {osc:osc, env:env, mul:0.15}).play();
 
+const START_NOTE = 69;
+
 var Song = React.createClass({
   mixins: [SongStore.listenTo, State, Navigation],
   getInitialState() {
     return {
       song: SongStore.all(),
-      beat: 0
+      beat: 0,
+      resolution: [16, 16]
     };
   },
   componentDidMount() {
     this.interval = T('interval', {interval:100}, (count) => {
-      var beat = count % 16;
+      var beat = count % this.state.resolution[0];
       this.setState({ beat: beat });
       this.state.song.get(beat, Immutable.Map()).forEach((note, index) => {
         if (note) {
-          var noteNum  = 69 + index;
+          var noteNum  = START_NOTE + index;
           var velocity = 64;
           synth.noteOn(noteNum, velocity);
         }
@@ -52,22 +55,38 @@ var Song = React.createClass({
     actions.undo();
   },
   previewNote: _.debounce((beat, note) => {
-    synth.noteOn(note + 69, 80);
+    synth.noteOn(note + START_NOTE, 80);
   }, 100),
   noteOn(beat, note) {
     return this.state.song.getIn([beat, note]);
   },
+  resolutionY() {
+    this.setState({
+      resolution: [this.state.resolution[0], this.state.resolution[1] === 16 ? 32 : 16]
+    });
+  },
+  resolutionX() {
+    this.setState({
+      resolution: [this.state.resolution[0] === 16 ? 32 : 16, this.state.resolution[1]]
+    });
+  },
   render: function() {
     return <div>
         <div className='fill-pink fix-width keyline-all round'>
-          {_.range(0, 16).map((beat) => {
-            return <div key={'beat-' + beat} className={'flex-beat ' + (beat === this.state.beat ? ' beat-on' : '')}>
-            {_.range(0, 16).reverse().map((note) => {
+          {_.range(0, this.state.resolution[0]).map((beat) => {
+            return <div
+                key={'beat-' + beat}
+                className={'flex-beat ' +
+                    `resolution-${this.state.resolution[0]}` +
+                    (beat === this.state.beat ? ' beat-on' : '')}>
+            {_.range(0, this.state.resolution[1]).reverse().map((note) => {
                 return <div
-                    key={'beat-' + beat + '-note-' + note}
+                    key={`beat-${beat}-note-${note}`}
                     onMouseOver={this.previewNote.bind(this, beat, note)}
                     onClick={this.flipNote.bind(this, beat, note)}
-                    className={'love-note ' + (this.noteOn(beat, note) ? ' note-on' : '')}>
+                    className={'love-note ' +
+                        `resolution-${this.state.resolution[1]}` +
+                        (this.noteOn(beat, note) ? ' note-on' : '')}>
                 </div>
             })}
             </div>
@@ -78,6 +97,10 @@ var Song = React.createClass({
                 className={'icon undo ' + (!SongStore.hasUndo() ? 'quiet' : '')}></a>
             <a onClick={actions.redo}
                 className={'icon redo ' + (!SongStore.hasRedo() ? 'quiet' : '')}></a>
+            <a onClick={this.resolutionX}
+                className={'icon l-r-arrow'}></a>
+            <a onClick={this.resolutionY}
+                className={'icon u-d-arrow'}></a>
             <a onClick={actions.reset}
                 className={'icon trash'}></a>
         </div>
